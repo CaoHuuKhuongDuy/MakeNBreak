2c85acce7add13273033af5f3752cdc4b0a2b2db
Dao Hong Minh <10422051@student.vgu.edu.vn>
2025-01-11 21:30:42 +0100

    create new custom toggle button

---
+++ src/com/models/components/CustomToggleButton.java
@@ -0,0 +1,19 @@
+package com.models.components;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.ContentDisplay;
+
+public class CustomToggleButton extends ToggleButton {
+    public CustomToggleButton(String text, String imagePath) {
+        super(text);
+
+        Image image = new Image(imagePath);
+        ImageView imageView = new ImageView(image);
+        this.setGraphic(imageView);
+
+        this.getStylesheets().add("resources/assets/styles/Button.css");
+
+        this.setContentDisplay(ContentDisplay.CENTER);
+    }
+}
